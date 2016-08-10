bdbd428a87a70a96808d5d76b077ee629a5210ee
mohit4 <mohitkumar2801@gmail.com>
2016-08-10 22:50:01 +0530

    added code for binary no representation recursively

---
+++ binary.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class binary{
+
+	//recursive function to print binary no
+	static void printBin(int n){
+		if(n==1||n==0){
+			System.out.print(n);
+			return;
+		}
+		int r = n%2;
+		printBin(n/2);
+		System.out.print(r);
+	}
+
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter a no : ");
+		printBin(s.nextInt());
+		System.out.println();
+		s.close();
+	}
+
+}
