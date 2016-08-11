170e7bb5e4949962a42b2709d18938f65d931eb2
mohit4 <mohitkumar2801@gmail.com>
2016-08-12 01:46:10 +0530

    something on enumerations

---
+++ onEnum.java
@@ -0,0 +1,11 @@
+public class onEnum{
+	
+	enum Color{
+		Red,Green,White,Yellow,Blue,Magenta,Cyan
+	}
+
+	public static void main(String[] args) {
+		Color c = Color.White;
+		System.out.println("Color is "+c);
+	}
+}
